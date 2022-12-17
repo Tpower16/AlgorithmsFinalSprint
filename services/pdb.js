7823e21933960373be6621856c844c5fc49b6d55
Terrance Power <Terrance.Power@keyin.com>
2022-12-17 07:30:13 -0330

    Create pdb.js

---
+++ services/pdb.js
@@ -0,0 +1,9 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "car_db",
+    password: "Keyin2022",
+    port: 5432,
+});
+module.exports = pool;
